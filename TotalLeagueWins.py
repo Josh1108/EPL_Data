f833e33d65da430a4f2778f761dc169b44f46aa9
Jushaan Kalra <Jushaan18@gmail.com>
2019-08-04 07:15:44 +0530

    first commit

---
+++ TotalLeagueWins.py
@@ -0,0 +1,76 @@
+# libraries
+import numpy as np
+import pandas as pd
+import csv
+import cv2
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import scipy
+from imageio import imread
+
+def connectpoints(x1,y1,x2,y2):
+    plt.plot([x1,x2],[y1,y2],'-')
+
+def image_plot(counts, images, spacing=0, X_label='x', Y_label='y',Title = 'Title'):
+    # Iterate through images and data, autoscaling the width to
+    # the aspect ratio of the image
+    height = 2
+    for i, (count, img) in enumerate(zip(counts, images)):
+        AR = img.shape[1] / img.shape[0]
+        width = height
+        left = width*i + spacing*i
+        right = left + width
+        for j in range(count):
+            plt.imshow(img, extent=[left, right, height*j, height*(j+1)])
+        plt.text((left + right - 0.7) / 2 , height*count + 0.5, list_of_winners[i],fontsize = 15, rotation = 90)
+
+    # Set size of figure
+    plt.rcParams["figure.figsize"] = (25,25)
+
+    # set font
+    plt.rcParams['font.family'] = 'sans-serif'
+    plt.rcParams['font.sans-serif'] = 'Helvetica'
+
+    # set the style of the axes and the text color
+    plt.rcParams['axes.edgecolor']='#333F4B'
+    plt.rcParams['axes.linewidth']=0.8
+    plt.rcParams['xtick.color']='#333F4B'
+    plt.rcParams['ytick.color']='#333F4B'
+    plt.rcParams['text.color']='#333F4B'
+
+    # Set x,y limits and labels on plot window
+    plt.xlim(0, right)
+    plt.ylim(0, max(counts)*2)
+    plt.xticks(np.arange(1,108,108/len(years)), years, rotation = 90)
+    plt.yticks(np.arange(1,32.5,2.2), range(1,14))
+    plt.tick_params(axis='both', length = 0)
+    plt.xlabel(X_label)
+    plt.ylabel(Y_label)
+    plt.title(Title)
+    plt.savefig('ass.png', dpi=300, bbox_inches='tight')
+
+# Importing Table Req to create graph
+df = pd.read_csv('/home/arshreality/Downloads/winners.csv')
+
+# Assign label names for plotting
+X_label1 = 'Years'
+Y_label1 = 'Premier League Wins'
+Title1 = 'Total Premier League Wins by Team and Year'
+
+# Read in logo images and set them to equal sqaures
+man_united_logo = cv2.resize(imread('/home/arshreality/Downloads/logos/mu.png'), (400, 400))
+man_city_logo = cv2.resize(imread('/home/arshreality/Downloads/logos/mc.png'), (400, 400))
+chelsea_logo = cv2.resize(imread('/home/arshreality/Downloads/logos/c.png'), (400, 400))
+arsenal_logo = cv2.resize(imread('/home/arshreality/Downloads/logos/a.png'), (400, 400))
+leicester_logo = cv2.resize(imread('/home/arshreality/Downloads/logos/lc.png'), (400, 400))
+blackburn_logo = cv2.resize(imread('/home/arshreality/Downloads/logos/br.png'), (400, 400))
+
+# Convert each column of dataframe to lists for plotting
+count = df['Count'].tolist()
+list_of_winners = df['Winner'].tolist()
+years = df['Year'].tolist()
+
+# Creating a list of logos
+logos = [man_united_logo,man_united_logo,blackburn_logo,man_united_logo,man_united_logo,arsenal_logo,man_united_logo,man_united_logo,man_united_logo,arsenal_logo,man_united_logo,arsenal_logo,chelsea_logo,chelsea_logo,man_united_logo,man_united_logo,man_united_logo,chelsea_logo,man_united_logo,man_city_logo,man_united_logo,man_city_logo,chelsea_logo,leicester_logo,chelsea_logo,man_city_logo,man_city_logo]
+
+image_plot(count, logos, spacing = 2, X_label = X_label1, Y_label = Y_label1, Title = Title1)
---
+++ TotalMatchWins.py
@@ -0,0 +1,103 @@
+# libraries
+import numpy as np
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+from matplotlib import rc
+import matplotlib.patches as mpatches
+
+# Importing Table Req to create graph
+EPL_set =pd.read_csv('https://drive.google.com/uc?export=download&id=1TcA1Aspas3z3Nc-288IRcNB741Xl14Nv')
+
+List_of_teams = set(EPL_set['HomeTeam'].values)
+dic_of_wins= {el:0 for el in List_of_teams}
+dic_of_wins # Created a dictionary of teams and their wins. Now we'll take the wins from table and as we
+            # Traverse through it we'll update it here
+dic_of_losses={el:0 for el in List_of_teams}
+dic_of_draws={el:0 for el in List_of_teams}
+
+# Updating dictionary as per wins losses and draws
+with open('EPL_Set.csv','r') as file:
+  data =csv.reader(file)
+  header= next(data)
+  for row in data:
+    if row[6]=='A' :
+      dic_of_wins[str(row[3])]+=1
+      dic_of_losses[str(row[2])]+=1
+    elif row[6]=='H' :
+      dic_of_wins[str(row[2])]+=1
+      dic_of_losses[str(row[3])]+=1
+    else:
+      dic_of_draws[str(row[2])]+=1
+      dic_of_draws[str(row[3])]+=1
+dic_of_draws.items()
+
+
+# set font
+plt.rcParams['font.family'] = 'sans-serif'
+plt.rcParams['font.sans-serif'] = 'Helvetica'
+
+# set the style of the axes and the text color
+plt.rcParams['axes.edgecolor']='#333F4B'
+plt.rcParams['axes.linewidth']=0.8
+plt.rcParams['xtick.color']='#333F4B'
+plt.rcParams['ytick.color']='#333F4B'
+plt.rcParams['text.color']='#333F4B'
+
+mydicts = [dic_of_draws,dic_of_wins, dic_of_losses]
+df = pd.concat([pd.Series(d) for d in mydicts], axis=1).fillna(0)
+df.reset_index(inplace =True)
+list(df[0].values)
+df.columns=['Name','draws','wins','losses']
+list(df['wins'].values)
+
+df['Total']= df['wins']+df['draws'] + df['losses']
+df
+df.sort_values(by=['Total','wins'],ascending = True,inplace =True)
+df.reset_index(inplace =True)
+df.drop('index',inplace =True,axis=1)
+
+# we first need a numeric placeholder for the y axis
+my_range=list(range(1,len(df.index)+1))
+my_range
+fig, ax = plt.subplots(figsize=(10,15))
+
+# create for each expense type an horizontal line that starts at x = 0 with the length
+# represented by the specific expense percentage value.
+plt.hlines(y=my_range, xmin=0, xmax=df['wins'], color='#00ccb8', alpha=0.4, linewidth=6)
+plt.hlines(y=my_range, xmin=df['wins'], xmax=df['wins']+df['draws'], color='#007ACC', alpha=0.4, linewidth=6)
+plt.hlines(y=my_range, xmin=df['wins']+df['draws'], xmax=df['wins']+df['draws']+df['losses'], color='#0014cc', alpha=0.4, linewidth=6)
+
+
+wins_label = mpatches.Patch(color='#00ccb8', label='wins')
+draws_label = mpatches.Patch(color='#007ACC', label='draws')
+loss_label = mpatches.Patch(color='#0014cc', label='losses')
+ax.legend(handles=[wins_label,draws_label,loss_label],loc =7)
+
+
+# create for each expense type a dot at the level of the expense percentage value
+plt.plot(df['wins'], my_range, "o", markersize=6, color='#00ccb8', alpha=0.8)
+plt.plot(df['wins']+df['draws'], my_range, "o", markersize=6, color='#007ACC', alpha=0.8)
+plt.plot(df['wins']+df['draws']+df['losses'], my_range, "o", markersize=6, color='#0014cc', alpha=0.8)
+
+# set labels
+ax.set_xlabel('Number of Wins, Losses & Draws', fontsize=15, fontweight='black', color = '#333F4B')
+ax.set_ylabel('')
+# set axis
+ax.tick_params(axis='both', which='major', labelsize=12)
+plt.yticks(my_range, df['Name'])
+plt.title('Wins, Losses & Draws for each team')
+
+# change the style of the axis spines
+ax.spines['top'].set_color('none')
+ax.spines['right'].set_color('none')
+ax.spines['left'].set_smart_bounds(True)
+ax.spines['bottom'].set_smart_bounds(True)
+plt.axvline(x=200,ymax = 0.96,color = 'black',linewidth = 0.5, alpha =0.5)
+plt.axvline(x=400,ymax = 0.96,color = 'black',linewidth = 0.5, alpha =0.5)
+plt.axvline(x= 600,ymax = 0.96,color = 'black',linewidth = 0.5, alpha =0.5)
+plt.axvline(x= 800,ymax = 0.96,color = 'black',linewidth = 0.5, alpha =0.5)
+# set the spines position
+ax.spines['bottom'].set_position(('axes', 0))
+ax.spines['left'].set_position(('axes', 0.015))
+plt.savefig('hist2.png', dpi=300, bbox_inches='tight')
